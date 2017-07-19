6def301d0d86f00207f380e2624791faf6152fd8
Brandon Sollins <brandon.sollins@izea.com>
2017-07-19 00:25:35 -0400

    routing stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :musicians, path: "/workflow/musicians"
-  resources :sign_up, path: "/workflow/sign_up"
+  resources :musicians
+  resources :sign_up
 end
