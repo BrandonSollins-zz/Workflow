4d5552d8bd4503bc08e4a13dce06ae344c694310
Brandon Sollins <brandon.sollins@izea.com>
2017-08-27 16:00:57 -0400

    Environment error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.enable_dependency_loading = true
+  #config.autoload_paths << Rails.root.join('lib')
 
   # Code is not reloaded between requests.
   config.cache_classes = true
