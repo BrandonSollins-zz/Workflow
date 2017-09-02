5b87f228e70cf3fdfcbde14b03723871ee450681
Brandon Sollins <brandon.sollins@izea.com>
2017-09-02 15:03:56 -0400

    Better ordering

--- app/controllers/songwriter_controller.rb
+++ app/controllers/songwriter_controller.rb
@@ -38,45 +38,45 @@ class SongwriterController < ApplicationController
       all_data: {}
 
     }
-    td = Songwriter.where("available_times = ''").order([:name, :time_zone])
+    td = Songwriter.where("available_times = ''").order([:time_zone, :name])
     all_data[:all_data]["Not Specified"] = {count: td.count(), emails: td.map{ |x| "#{x.name} - #{x.email} - #{x.time_zone}" }}
     possible_days.each do |day|
-      td = Songwriter.where("available_times like '%#{day}%'").order([:name, :time_zone])
+      td = Songwriter.where("available_times like '%#{day}%'").order([:time_zone, :name])
       all_data[:all_data][day] = {count: td.count(), emails: td.map{ |x| "#{x.name} - #{x.email} - #{x.time_zone}" }}
     end
     @cities.each do |city|
       all_data[:cities][city] = {}
-      td = Songwriter.where("city = '#{city}' AND available_times = ''").order([:name, :time_zone])
+      td = Songwriter.where("city = '#{city}' AND available_times = ''").order([:time_zone, :name])
       all_data[:cities][city]["Not Specified"] = {count: td.count(), emails: td.map{ |x| "#{x.name} - #{x.email} - #{x.time_zone}" }}
       possible_days.each do |day|
-        td = Songwriter.where("city = '#{city}' AND available_times like '%#{day}%'").order([:name, :time_zone])
+        td = Songwriter.where("city = '#{city}' AND available_times like '%#{day}%'").order([:time_zone, :name])
         all_data[:cities][city][day] = {count: td.count(), emails: td.map{ |x| "#{x.name} - #{x.email} - #{x.time_zone}" }}
       end
     end
     @regions.each do |region|
       all_data[:regions][region] = {}
-      td = Songwriter.where("region = '#{region}' AND available_times = ''").order([:name, :time_zone])
+      td = Songwriter.where("region = '#{region}' AND available_times = ''").order([:time_zone, :name])
       all_data[:regions][region]["Not Specified"] = {count: td.count(), emails: td.map{ |x| "#{x.name} - #{x.email} - #{x.time_zone}" }}
       possible_days.each do |day|
-        td = Songwriter.where("region = '#{region}' AND available_times like '%#{day}%'").order([:name, :time_zone])
+        td = Songwriter.where("region = '#{region}' AND available_times like '%#{day}%'").order([:time_zone, :name])
         all_data[:regions][region][day] = {count: td.count(), emails: td.map{ |x| "#{x.name} - #{x.email} - #{x.time_zone}" }}
       end
     end
     @countries.each do |country|
       all_data[:countries][country] = {}
-      td = Songwriter.where("country = '#{country}' AND available_times = ''").order([:name, :time_zone])
+      td = Songwriter.where("country = '#{country}' AND available_times = ''").order([:time_zone, :name])
       all_data[:countries][country]["Not Specified"] = {count: td.count(), emails: td.map{ |x| "#{x.name} - #{x.email} - #{x.time_zone}" }}
       possible_days.each do |day|
-        td = Songwriter.where("country = '#{country}' AND available_times like '%#{day}%'").order([:name, :time_zone])
+        td = Songwriter.where("country = '#{country}' AND available_times like '%#{day}%'").order([:time_zone, :name])
         all_data[:countries][country][day] = {count: td.count(), emails: td.map{ |x| "#{x.name} - #{x.email} - #{x.time_zone}" }}
       end
     end
     @time_zones.each do |tz|
       all_data[:time_zones][tz] = {}
-      td = Songwriter.where("time_zone = '#{tz}' AND available_times = ''").order([:name, :time_zone])
+      td = Songwriter.where("time_zone = '#{tz}' AND available_times = ''").order([:time_zone, :name])
       all_data[:time_zones][tz]["Not Specified"] = {count: td.count(), emails: td.map{ |x| "#{x.name} - #{x.email} - #{x.time_zone}" }}
       possible_days.each do |day|
-        td = Songwriter.where("time_zone = '#{tz}' AND lower(available_times) like '%#{day}%'").order([:name, :time_zone])
+        td = Songwriter.where("time_zone = '#{tz}' AND lower(available_times) like '%#{day}%'").order([:time_zone, :name])
         all_data[:time_zones][tz][day] = {count: td.count(), emails: td.map{ |x| "#{x.name} - #{x.email} - #{x.time_zone}" }}
       end
     end
